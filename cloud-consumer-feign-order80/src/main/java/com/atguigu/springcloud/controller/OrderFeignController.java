fd24641f8d9e2137e6e3b811992171797b42d7b5
YINLakers <1320207218@qq.com>
2021-05-16 10:48:33 +0800

    调试mysql连接，Feign调用

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/controller/OrderFeignController.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/controller/OrderFeignController.java
@@ -1,13 +1,13 @@
 package com.atguigu.springcloud.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONPObject;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentFeignService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by chenpeng on 2020/11/4 11:45
@@ -20,14 +20,29 @@ public class OrderFeignController {
     private PaymentFeignService paymentFeignService;
 
     @GetMapping("/consumer/payment/get/{id}")
-    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         log.info("消费者调用查询方法");
         return paymentFeignService.getPaymentById(id);
     }
 
     @GetMapping("/consumer/payment/timeout")
-    public String paymentFeignTimeout(){
+    public String paymentFeignTimeout() {
         log.info("调用超时方法");
-       return paymentFeignService.paymentFeignTimeout();
+        return paymentFeignService.paymentFeignTimeout();
     }
+
+    @PostMapping("/consumer/payment/test")
+    public JSONObject test(@RequestBody JSONObject parameter) {
+
+        System.out.println("参数：" + parameter);
+        JSONObject object = new JSONObject();
+        object.put("name", "xiaomi");
+        object.put("age", "10");
+
+        log.info("消费者调用查询方法");
+
+        return object;
+
+    }
+
 }
